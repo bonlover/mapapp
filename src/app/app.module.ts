f7952da97b2f1a9b091164f19bfc8d6db9be8b34
bonlover <yungyalsen@gmail.com>
2022-06-28 19:04:17 -0400

    Adding initial commit angular project with partials components, location and login form components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { EmailComponent } from './_partials/components/email/email.component';
+import { TextComponent } from './_partials/components/text/text.component';
+import { DropdownComponent } from './_partials/components/dropdown/dropdown.component';
+import { FileComponent } from './_partials/components/file/file.component';
+import { TextareaComponent } from './_partials/components/textarea/textarea.component';
+import { LoginComponent } from './components/login/login.component';
+import { LocationComponent } from './components/location/location.component';
+import { CheckboxComponent } from './_partials/components/checkbox/checkbox.component';
+import { RadiobuttonComponent } from './_partials/components/radiobutton/radiobutton.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EmailComponent,
+    TextComponent,
+    DropdownComponent,
+    FileComponent,
+    TextareaComponent,
+    LoginComponent,
+    LocationComponent,
+    CheckboxComponent,
+    RadiobuttonComponent
   ],
   imports: [
     BrowserModule,
